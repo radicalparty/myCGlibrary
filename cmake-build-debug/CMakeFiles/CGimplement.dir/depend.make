2f185c5724b8b57a3ae3f3387db430f206df00b1
pnuth <85662273+radicalparty@users.noreply.github.com>
2024-01-12 15:50:56 +0900

    Delete cmake-build-debug directory

--- cmake-build-debug/CMakeFiles/CGimplement.dir/depend.make
+++
@@ -1,2 +0,0 @@
-# Empty dependencies file for CGimplement.
-# This may be replaced when dependencies are built.
--- cmake-build-debug/CMakeFiles/CGimplement.dir/update.cpp.o.d
+++
@@ -1,3 +0,0 @@
-CMakeFiles/CGimplement.dir/update.cpp.o: \
- /mnt/c/Users/자라/CLionProjects/CGimplement/update.cpp \
- /usr/include/stdc-predef.h
